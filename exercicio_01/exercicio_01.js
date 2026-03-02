0c7425281113dab2302d9acdb82a8976874ebb66
User <user@MacBook-Pro-de-User.local>
2026-03-02 11:17:46 -0800

    bonus nos exercícios 1 e 2

--- exercicio_01/exercicio_01.js
+++ exercicio_01/exercicio_01.js
@@ -8,5 +8,7 @@ salario = leia.questionFloat("Digite seu salário: R$ ");
 abono = leia.questionFloat("Digite o valor do abono: R$ ");
 
 novoSalario = salario + abono;
+salarioDiferenca = novoSalario - salario;
 
 console.log("Seu novo salário é: R$ " + novoSalario);
+console.log("A diferença do salário é: R$ " + salarioDiferenca.toFixed(2)); // adicao do exercício
--- exercicio_02/exercicio_02.js
+++ exercicio_02/exercicio_02.js
@@ -5,6 +5,7 @@ let nota2;
 let nota3;
 let nota4;
 let mediaFinal;
+let aprovacao;
 
 nota1 = leia.questionFloat("Digite sua primeira nota: ");
 nota2 = leia.questionFloat("Digite sua segunda nota: ");
@@ -12,5 +13,7 @@ nota3 = leia.questionFloat("Digite sua terceira nota: ");
 nota4 = leia.questionFloat("Digite sua quarta nota: ");
 
 mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+aprovacao = mediaFinal >= 7 ? "Aprovado" : "Reprovado"; // adicao do exercício
 
 console.log("Sua média final é: " + mediaFinal.toFixed(1));
+console.log("Situação: " + aprovacao);

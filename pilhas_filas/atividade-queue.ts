d2865dda7770525a76c53514efbe3821c9b0ef6c
User <user@MacBook-Pro-de-User.local>
2026-03-06 07:41:37 -0800

    Atividades 06/03/2026

--- pilhas_filas/atividade-queue.ts
+++ pilhas_filas/atividade-queue.ts
@@ -1,3 +1,62 @@
+import leia = require("readline-sync");
 import { Queue } from "./Queue";
 
-const fila = new Queue();
+const fila = new Queue<string>();
+
+let opcao: number;
+let nome: string;
+let deveriaContinue: boolean = true;
+
+do {
+    console.log("**************************************");
+    console.log("");
+    console.log("1 - Adicionar Cliente na Fila");
+    console.log("2 - Listar todos os Clientes");
+    console.log("3 - Retirar Cliente da Fila");
+    console.log("0 - Sair");
+    console.log("");
+    console.log("**************************************");
+    opcao = leia.questionInt("Digite uma opção: ");
+    switch (opcao) {
+        case 1:
+            console.log("");
+            nome = String(leia.question("Digite o nome do cliente: "));
+            fila.enqueue(nome);
+            fila.printQueue();
+            console.log("Cliente adicionado à fila.");
+            console.log("");
+            break;
+        case 2:
+            console.log("");
+            console.log("Lista de Clientes na Fila:");
+            fila.printQueue();
+            console.log("");
+            break;
+        case 3:
+            if (fila.isEmpty() && opcao === 3) {
+                console.log("");
+                console.log("A fila está vazia.");
+                console.log("");
+            } else {
+                console.log("");
+                let clienteChamado = fila.peek();
+                console.log(`Cliente ${clienteChamado} chamado!`);
+                fila.dequeue();
+                console.log("Fila atualizada:");
+                fila.printQueue();
+                console.log("");
+            }
+            break;
+        case 0:
+            console.log("");
+            deveriaContinue = false;
+            console.log("Programa Finalizado!");
+            console.log("");
+            break;
+        default:
+            console.log("");
+            console.log("Opção inválida. Digite uma opção válida.");
+            console.log("");
+            break;
+    } 
+} while (deveriaContinue);
--- pilhas_filas/atividade-stack.ts
+++ pilhas_filas/atividade-stack.ts
@@ -1,7 +1,63 @@
+import leia = require("readline-sync");
 import { Stack } from "./Stack";
 
-const pilha = new Stack<number>();
 
-pilha.push(7);
+const pilha = new Stack<string>();
 
-pilha.printStack();
+let opcao: number;
+let nome: string;
+let deveriaContinue: boolean = true;
+
+do {
+    console.log("**************************************");
+    console.log("");
+    console.log("1 - Adicionar Livro na pilha");
+    console.log("2 - Listar todos os Livros");
+    console.log("3 - Retirar Livro da pilha");
+    console.log("0 - Sair");
+    console.log("");
+    console.log("**************************************");
+    opcao = leia.questionInt("Digite uma opção: ");
+
+       switch (opcao) {
+            case 1:
+                console.log("");
+                nome = String(leia.question("Digite o nome do Livro: "));
+                pilha.push(nome);
+                pilha.printStack();
+                console.log("Livro adicionado!");
+                console.log("");
+                break;
+            case 2:
+                console.log("");
+                console.log("Lista de Livros na Pilha:");
+                pilha.printStack();
+                console.log("");
+                break;
+            case 3:
+                if (pilha.isEmpty() && opcao === 3) {
+                    console.log("");
+                    console.log("A Pilha está vazia.");
+                    console.log("");
+                } else {
+                    console.log("");
+                    let livroRetirado = pilha.pop();
+                    console.log(`Livro ${livroRetirado} retirado da pilha.`);
+                    console.log("Pilha atualizada:");
+                    pilha.printStack();
+                    console.log("");
+                }
+                break;
+            case 0:
+                console.log("");
+                deveriaContinue = false;
+                console.log("Programa Finalizado!");
+                console.log("");
+                break;
+            default:
+                console.log("");
+                console.log("Opção inválida. Digite uma opção válida.");
+                console.log("");
+                break;
+        } 
+    } while (deveriaContinue);
